4cee86085011186d2c527b10ffd30bc7e82e1292
faithchan <hannahfaith75@gmail.com>
2022-01-30 15:36:43 +0800

    Navbar created

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import '../styles/globals.css'
+import Navbar from '../components/Navbar'
 
 
 function MyApp() {
   return <div className="bg-bgimg bg-cover">
+    <Navbar />
+
     <h1 className="font-header">Hi, this is from MyApp</h1>
     
   </div>
