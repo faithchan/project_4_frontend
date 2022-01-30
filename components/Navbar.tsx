09e58dc09353ec3ac95c3fcee86af6f7b98ccf9c
faithchan <hannahfaith75@gmail.com>
2022-01-30 15:53:37 +0800

    search bar done, navbar for non users done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import logo from "../public/ArkivLogo.svg"
+import Search from './Search'
 
 const Navbar = () => {
     return (
@@ -9,8 +10,9 @@ const Navbar = () => {
         <nav className="flex pt-10 px-20 place-content-between">
         <span className="mt-2 w-24"><Link href="/"><Image src={logo} alt="Logo" className="h-10 ml-8 mb-6"/></Link></span>
          <ul className="flex items-right  mt-2 h-full tracking-wider">
-      <li className="mx-10 "><Link href="/login">LOGIN</Link></li>
-      <li className="mx-10 "><Link href="/explore">EXPLORE</Link></li>
+      <li className="mx-10 mt-2"><Link href="/login">LOGIN</Link></li>
+      <li className="ml-10 mr-20 mt-2"><Link href="/explore">EXPLORE</Link></li>
+      <Search />
       </ul>
       </nav>
       </div>
---
+++ components/Search.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Search = () => {
+    return (
+        <div className="relative text-gray-400">
+        <input type="search" name="search" placeholder="Search" className="bg-white h-10 pl-5 rounded-full text-sm focus:outline-none"/>
+        <button type="submit" className="absolute right-0 top-0 mt-3 pr-4 ">
+        ok
+        </button>
+        </div>
+   
+    )
+}
+
+export default Search
