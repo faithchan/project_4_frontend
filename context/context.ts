2a09ff37db1206c22c9e554308cc049606b0a7b8
faithchan <hannahfaith75@gmail.com>
2022-02-01 16:38:03 +0800

    cleaned up context

---
+++ context/context.ts
@@ -0,0 +1,13 @@
+import React, { createContext } from 'react'
+
+interface userLoginStatus {
+   login:boolean,
+   walletID:string,
+   designerState:boolean
+}
+
+
+
+const userContext = createContext<userLoginStatus | null>(null);
+
+export default userContext
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,22 +2,36 @@ import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
 import Static from '../components/Static'
-
 import Head from 'next/head'
+import userContext from "../context/context"
+import {useState} from 'react'
+
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-
+  const [login, setLogin] = useState(true)
+  const [designerState, setDesignerState] = useState(false)
+  const [walletID, setWalletID] =useState("123d")
+  const userLoginData={
+    login: login,
+    designerState: designerState,
+    walletID:walletID
+  }
+  console.log(userLoginData)
+  
   return (
 
     <div>
+      <userContext.Provider value={userLoginData}>
         <Head>
           <title>ARKIV</title>
         </Head>
+        
         <Static>
           <Component {...pageProps} />
-          </Static>
-       
+        </Static>
+        {}
+      </userContext.Provider>
     </div>
   )
 }
