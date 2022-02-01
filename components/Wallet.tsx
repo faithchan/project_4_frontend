2a21e34e021a774735b3aaf06fe7ac690430971f
chloet <chloet@chloetklaytns-MacBook-Pro.local>
2022-02-01 15:42:23 +0800

    added ipfs dependency, wallet and contract components for testing

---
+++ components/Wallet.tsx
@@ -0,0 +1,10 @@
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+import { useState, useEffect } from "react";
+
+const Wallet = () => {
+  const [walletAddress, setWalletAddress] = useState("");
+  return <div>test wallet</div>;
+};
+
+export default Wallet;
---
+++ pages/contracts.tsx
@@ -0,0 +1,12 @@
+import Wallet from "../components/Wallet";
+
+const Contracts = () => {
+  return (
+    <div className="text-white">
+      test contract
+      <Wallet />
+    </div>
+  );
+};
+
+export default Contracts;
