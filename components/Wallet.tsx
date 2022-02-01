651d85ca3660e8ba5cdb66f0ec76f0556918847b
chloet <chloet@chloetklaytns-MacBook-Pro.local>
2022-02-01 16:06:53 +0800

    added connect and disconnect wallet functions

--- components/Wallet.tsx
+++ components/Wallet.tsx
@@ -4,7 +4,40 @@ import { useState, useEffect } from "react";
 
 const Wallet = () => {
   const [walletAddress, setWalletAddress] = useState("");
-  return <div>test wallet</div>;
+  const [connected, setConnected] = useState<Boolean>(false);
+
+  const connectWallet = async () => {
+    if (typeof window.ethereum !== "undefined") {
+      console.log("MetaMask is present");
+      const web3Modal = new Web3Modal();
+      const connection = await web3Modal.connect();
+      const provider = new ethers.providers.Web3Provider(connection);
+      const signer = provider.getSigner();
+      const connectedAddress = await signer.getAddress();
+      console.log("Connected Wallet: ", connectedAddress);
+      setWalletAddress(connectedAddress);
+      setConnected(true);
+    } else {
+      alert("Please install Metamask");
+    }
+  };
+
+  const disconnectWallet = async () => {
+    setWalletAddress("");
+    setConnected(false);
+  };
+
+  return (
+    <div>
+      {connected ? (
+        <>
+          <button onClick={disconnectWallet}>Disconnect</button>
+        </>
+      ) : (
+        <button onClick={connectWallet}>Connect Wallet</button>
+      )}
+    </div>
+  );
 };
 
 export default Wallet;
