c1e74fc8faf8a554ac2850f76350e262ec44ab8a
faithchan <hannahfaith75@gmail.com>
2022-02-05 15:27:33 +0800

    fixed TradeCard and flex around the cards

--- components/TradeCard.tsx
+++ components/TradeCard.tsx
@@ -3,25 +3,30 @@ import React from 'react'
 const TradeCard = () => {
     return (
         <div>
-            <div className="w-96 p-8 bg-purple opacity-80 rounded-3xl shadow-xl hover:shadow-2xl hover:scale-105 transition-all transform duration-500">
+            <div className="w-full p-8 bg-purple opacity-80 rounded-3xl shadow-xl hover:shadow-2xl hover:scale-105 transition-all transform duration-500 ">
 
-              <div className="flex justify-between p-6">
-              <span className="font-MT font-semibold text-xs leading-loose mr-10">
-                <p>Patient: </p>
-                <p>Date:</p>
-                <p>Doctor: </p>
-                <p>Type:</p>
-                <p>Notes:</p>
+              <div className="flex justify-between space-x-14">
+
+              <span className="font-MT font-semibold text-left leading-loose">
+              <img className="w-44 h-44 object-cover rounded-3xl" src="https://images.unsplash.com/photo-1617791160505-6f00504e3519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1856&q=80" alt="" />
+              <p className="text-gold mt-4 text-md font-header tracking-widest">SILO</p>
               </span>
 
-              <span className="font-MT font-semibold text-right leading-loose">
-              
-              <button className="bg-blue-400 hover:bg-blue-450 text-white font-semibold tracking-widest font-MT py-2 px-4 rounded-full text-xs mx-auto " onClick={()=>window.open("https://us05web.zoom.us/j/5314369063?pwd=SEZzVVR5VE5hYW9XVW4xa1o4SElRdz09")}>Join Zoom</button>
-                <ul className="text-xs flex mt-4" >
-                <li><button className="bg-red rounded-full w-4 h-4 mr-2 " /></li>
-                <p className="text-left cursor-pointer hover:underline">Cancel Appointment</p>
-                </ul>
-              </span>
+              <span > 
+                <p className="text-gold text-sm font-header tracking-widest">Designed by</p>
+                  <span className="flex space-x-4 mr-6">
+                    <img className="w-16 h-16 object-cover rounded-full mr-4 mt-6 mb-4" src="https://api.lorem.space/image/face?w=200&h=200&hash=bart89fe" alt="" />
+                        <span className="my-auto">
+                        <p className="text-center text-gold font-header text-xs   tracking-widest">FAKURIAN</p>
+                        <hr className="border-gold border my-2"></hr>
+                        <p className="text-center text-white font-body text-xs  tracking-widest">View Profile</p>
+                        </span> 
+                    </span>
+
+                    <p className="text-gold text-sm font-header tracking-widest mt-2">Owned by</p>
+                    <p className="text-gold text-xs font-header tracking-widest mt-2">RACHEL LEE</p>
+              </span> 
+         
               </div>
  
             </div>
--- pages/trades.tsx
+++ pages/trades.tsx
@@ -3,8 +3,10 @@ import TradeCard from '../components/TradeCard'
 
 const trades = () => {
     return (
-        <div className="my-20">
-            <div className="flex flex-wrap space-x-10 justify-center">
+        <div className="my-20 mx-32">
+            <div className="flex flex-wrap gap-10 justify-center">
+            <TradeCard />
+            <TradeCard />
             <TradeCard />
             <TradeCard />
             </div>
