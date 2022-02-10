88ea2f6ce8875b8c24f427d63644aad7859e33b6
faithchan <hannahfaith75@gmail.com>
2022-02-10 13:15:49 +0800

    logic for the list nft token modal done and onsubmit prevents default refreah

--- components/ListNFTToken.tsx
+++ components/ListNFTToken.tsx
@@ -6,11 +6,19 @@ interface uploadProps{
 
 const ListNFTToken = (props:uploadProps) => {
     const [showRoyalty, setShowRoyalty] = useState(true)
-    const [showList, setShowList] = useState(true)
+    const [showList, setShowList] = useState(false)
+    const [showContBtn, setShowContBtn] = useState(false)
+    const [showRoyaltyBtn, setShowRoyaltyBtn] = useState(true)
 
-    const royaltyHandler =()=>{
+    const royaltyHandler =(e: React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
         setShowRoyalty(false);
         setShowList(true);
+    }
+    const listTokenHandler =(e: React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
+        setShowList(false)
+        props.setListNFTModal(false)
     }
     
     return (
@@ -18,7 +26,7 @@ const ListNFTToken = (props:uploadProps) => {
     <div className="absolute bg-black opacity-80 inset-0 z-0"></div>
         <div className="w-full  max-w-lg p-5 relative mx-auto my-auto rounded-xl shadow-lg  bg-white">
 
-            <form className="">
+            {showRoyalty?<form className="" onSubmit={royaltyHandler}>
                 <div className="text-center p-5 flex-auto justify-center">
                     <p className="text-2xl text-gold font-header px-8">Set Royalty</p>    
                 </div>
@@ -27,7 +35,7 @@ const ListNFTToken = (props:uploadProps) => {
                 <label className="block mb-1 md:text-sm text-xs text-gray-400 font-body">*In Percent (%)</label>
                 <input
                     type="number"
-                    className="bg-gray-100 px-4 py-2 border text-gray-600 border-gray-400 outline-none rounded-md w-full mt-2" placeholder="2%" min="0.01" max="99"
+                    className="bg-gray-100 px-4 py-2 border text-gray-600 border-gray-400 outline-none rounded-md w-full mt-2" placeholder="2%" min="1" max="99"
                     required
                 />
                 </div>
@@ -36,7 +44,10 @@ const ListNFTToken = (props:uploadProps) => {
                     <button className="mb-2 md:mb-0 bg-white px-5 py-2 text-xs shadow-sm font-header tracking-wider border text-gold rounded-full hover:shadow-lg hover:bg-gray-100" onClick={()=>props.setListNFTModal(false)}>
                         Cancel
                     </button>
-                    <button className="mb-2 md:mb-0 bg-gold px-5 py-2 text-xs shadow-sm  font-header tracking-wider text-white rounded-full hover:shadow-lg " onClick={royaltyHandler}>Set Royalty</button>
+
+                    {showRoyaltyBtn?<button className="mb-2 md:mb-0 bg-gold px-5 py-2 text-xs shadow-sm  font-header tracking-wider text-white rounded-full hover:shadow-lg " onClick={()=>{setShowContBtn(true); setShowRoyaltyBtn(false)}}>Set Royalty</button>:""}
+                    
+                    {showContBtn?<button className="mb-2 md:mb-0 bg-gold px-5 py-2 text-xs shadow-sm  font-header tracking-wider text-white rounded-full hover:shadow-lg " type="submit">Continue to List</button>:""}
                 </div>
 
                 {/* <div className="p-3 mt-2 text-center space-x-4 md:block">
@@ -45,7 +56,38 @@ const ListNFTToken = (props:uploadProps) => {
                     </button>
                     <button className="mb-2 md:mb-0 bg-gold px-5 py-2 text-xs shadow-sm  font-header tracking-wider text-white rounded-full hover:shadow-lg ">Approve and List</button>
                 </div> */}
-                </form>
+                </form>:""}
+                {/* Next Form */}
+                {showList?<form className="" onSubmit={listTokenHandler}>
+                <div className="text-center p-5 flex-auto justify-center">
+                    <p className="text-2xl text-gold font-header px-8">Enter Price</p>    
+                </div>
+
+                <div className="px-20">
+                <label className="block mb-1 md:text-sm text-xs text-gray-400 font-body">*In Eth</label>
+                <input
+                    type="number"
+                    className="bg-gray-100 px-4 py-2 border text-gray-600 border-gray-400 outline-none rounded-md w-full mt-2" placeholder="1 Eth" min="1" 
+                    required
+                />
+                </div>
+
+                <div className="p-3 mt-2 text-center space-x-4 md:block">
+                    <button className="mb-2 md:mb-0 bg-white px-5 py-2 text-xs shadow-sm font-header tracking-wider border text-gold rounded-full hover:shadow-lg hover:bg-gray-100" onClick={()=>props.setListNFTModal(false)}>
+                        Cancel
+                    </button>
+                    <button className="mb-2 md:mb-0 bg-gold px-5 py-2 text-xs shadow-sm  font-header tracking-wider text-white rounded-full hover:shadow-lg" type="submit">List Token</button>
+                </div>
+
+                {/* <div className="p-3 mt-2 text-center space-x-4 md:block">
+                    <button className="mb-2 md:mb-0 bg-white px-5 py-2 text-xs shadow-sm font-header tracking-wider border text-gold rounded-full hover:shadow-lg hover:bg-gray-100">
+                        List Later
+                    </button>
+                    <button className="mb-2 md:mb-0 bg-gold px-5 py-2 text-xs shadow-sm  font-header tracking-wider text-white rounded-full hover:shadow-lg ">Approve and List</button>
+                </div> */}
+                </form>:""
+
+                }
             </div>
         </div>
     )
