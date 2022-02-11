01334b345bb1b6f6943906760ea831b28286e255
faithchan <hannahfaith75@gmail.com>
2022-02-11 14:43:28 +0800

    added dynamic page to view other user's profile

---
+++ pages/user/[id].tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+
+const Username = () => {
+    const router = useRouter()
+    const { id } = router.query
+  
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default [id]
