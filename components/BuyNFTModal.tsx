ddd080498979baf810aab78384737552a38880fe
faithchan <hannahfaith75@gmail.com>
2022-02-12 13:40:22 +0800

    successfully added the success logic to purchase nft

--- components/BuyNFTModal.tsx
+++ components/BuyNFTModal.tsx
@@ -1,12 +1,16 @@
+import {useState} from "react"
 interface buyProps {
   buyModal: boolean
   setBuyModal: (a: boolean) => void
 }
 
 const BuyNFTModal = (props: buyProps) => {
+  const [showPurchase, setShowPurchase] =useState(true)
+  const [success, setSuccess]= useState(false) 
   const purchaseHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    props.setBuyModal(false)
+    setShowPurchase(false)
+    setSuccess(true)
   }
   return (
     <div
@@ -15,7 +19,7 @@ const BuyNFTModal = (props: buyProps) => {
     >
       <div className="absolute bg-black opacity-80 inset-0 z-0"></div>
       <div className="w-full  max-w-lg p-5 relative mx-auto my-auto rounded-xl shadow-lg  bg-purple">
-        <form className="" onSubmit={purchaseHandler}>
+        {showPurchase?<form className="" onSubmit={purchaseHandler}>
           <div className="text-center p-5 flex-auto justify-center">
             <p className="text-2xl text-gold font-header px-8">Purchase NFT</p>
           </div>
@@ -47,7 +51,30 @@ const BuyNFTModal = (props: buyProps) => {
               Confirm Purchase
             </button>
           </div>
-        </form>
+        </form>:""}
+        {success?<div><div className="text-center p-5 flex-auto justify-center">
+          <p className="text-2xl text-gold font-header px-8">Success!</p>
+        </div>
+        <div className="flex justify-center px-6 pt-2 pb-4">
+            <img
+              className="w-56 h-56 object-cover rounded-3xl"
+              src="https://images.unsplash.com/photo-1617791160505-6f00504e3519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1856&q=80"
+              alt=""
+            />
+            <span className="ml-8 my-auto">
+              <p className="text-left text-xl font-header text-gold ">Title of the NFT</p>
+              <p className="text-left text-sm font-body text-gray-300 mt-2">Designed by Fakurian</p>
+              <p className="text-left text-sm font-body text-gray-300 mt-2">List Price 0.01 Eth</p>
+            </span>
+          </div>
+          <div className="p-3 mt-2 text-center space-x-4 md:block">
+          <button
+              className="mb-2 md:mb-0 bg-gold px-5 py-2 text-xs shadow-sm  font-header tracking-wider text-white rounded-full hover:shadow-lg "
+             
+            >
+              Back to Home
+            </button></div></div>:""
+        }
       </div>
     </div>
   )
