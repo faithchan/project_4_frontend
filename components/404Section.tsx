bd02a1cc14dd7ad542e365235a52bccbfa09e8f7
faithchan <hannahfaith75@gmail.com>
2022-02-18 11:18:19 +0800

    revised 404

--- components/404Section.tsx
+++ components/404Section.tsx
@@ -7,20 +7,28 @@ import { useRouter } from 'next/router'
 const Error = () => {
     const router= useRouter()
     return (
-        <div className="h-screen w-screen mt-40">
-               <div className="flex justify-center">
-                  <Image width={250} height={180} src={errorImg}></Image>
-                  </div>
-                  <div className="flex justify-center ">
-                  <button
-            className=" text-gold tracking-widest font-body px-4 text-xs mx-auto underline"
+<div className="h-screen w-screen flex items-center justify-center">
+	<div className="container flex flex-col md:flex-row items-center justify-center px-2 text-gray-700">
+    <div className="max-w-lg mr-24">
+      <Image width={300} height={180} src={errorImg}></Image>
+    </div>
+           <div className="max-w-md">
+            <p
+              className="text-2xl md:text-3xl font-body leading-normal text-gold"
+            >Sorry we couldn't find this page. </p>
+          <p className="my-4 font-body text-gray-300 text-sm">Contact our admin for more details.</p>
+          
+          <button
+            className="bg-gold hover:bg-blue-450 text-white font-semibold tracking-widest font-header py-2 px-4 rounded-full text-xs mx-auto "
             onClick={() => router.push('/')}
           >
             Back to Home
-          </button> 
-                  </div>
-                  
-      </div>
+          </button>
+    </div>
+      
+    
+  </div>
+</div>
    
     )
 }
