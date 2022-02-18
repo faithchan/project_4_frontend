0303c48dc654e264321b4155fea67d86a55ab7c7
faithchan <hannahfaith75@gmail.com>
2022-02-18 10:47:54 +0800

    404 done

--- components/404Section.tsx
+++ components/404Section.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import errorImg from "../public/404.svg"
 import Image from 'next/image'
+import { useRouter } from 'next/router'
+
 
 const Error = () => {
+    const router= useRouter()
     return (
-        <div className="h-screen w-screen  flex justify-center">
-               <div className="max-w-md">
-                  <Image width={250} src={errorImg}></Image>
+        <div className="h-screen w-screen mt-40">
+               <div className="flex justify-center">
+                  <Image width={250} height={180} src={errorImg}></Image>
+                  </div>
+                  <div className="flex justify-center ">
+                  <button
+            className=" text-gold tracking-widest font-body px-4 text-xs mx-auto underline"
+            onClick={() => router.push('/')}
+          >
+            Back to Home
+          </button> 
                   </div>
                   
       </div>
