8fcb1b9ea8c1eafa5f659b205b3c9a7bdb3d3bf4
chloet <chloet@chloetklaytns-MacBook-Pro.local>
2022-02-19 18:03:19 +0800

    removed wallet and contract pages, added edit page

---
+++ pages/edit.tsx
@@ -0,0 +1,10 @@
+import { useContext, useState, useEffect } from 'react'
+
+// redirect to login page if not logged in
+// username, email, password, wallet address, avatar
+
+const Edit = () => {
+  return <div>edit user</div>
+}
+
+export default Edit
