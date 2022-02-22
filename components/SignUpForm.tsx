69c14e568b00eff2157cf6a9d64a089fb1582f99
chloet <chloet@chloetklaytns-MacBook-Pro.local>
2022-02-22 20:40:02 +0800

    added image preview to signup form

--- components/SignUpForm.tsx
+++ components/SignUpForm.tsx
@@ -2,6 +2,7 @@ import { useForm } from 'react-hook-form'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { create } from 'ipfs-http-client'
+import { useState } from 'react'
 
 const url: string | any = 'https://ipfs.infura.io:5001/api/v0'
 const client = create(url)
@@ -16,6 +17,7 @@ type FormData = {
 
 const SignUpForm = () => {
   const router = useRouter()
+  const [imageURL, setImageURL] = useState('')
 
   const {
     register,
@@ -45,16 +47,6 @@ const SignUpForm = () => {
       setTimeout(() => {
         router.push('/login')
       }, 1000)
-      // const { username, email, walletAddress } = res.keyValue
-      // if (username) {
-      //   alert('Username already registered. Please try again.')
-      // }
-      // if (email) {
-      //   alert('Email already registered. Please try again.')
-      // }
-      // if (walletAddress) {
-      //   alert('Wallet address already registered. Please try again.')
-      // }
     } catch (err) {
       console.log(err)
     }
@@ -73,6 +65,7 @@ const SignUpForm = () => {
       )
       const url = `https://ipfs.infura.io/ipfs/${cid}`
       console.log('ipfs url: ', url)
+      setImageURL(url)
       setValue('avatar', url)
     } catch (e) {
       console.error('Error uploading file: ', e)
@@ -154,36 +147,40 @@ const SignUpForm = () => {
                 <label className="md:text-sm text-xs text-white font-body tracking-wider">
                   Upload Profile Picture
                 </label>
-                <div className="flex items-center justify-center w-full mt-2">
-                  <label className="flex flex-col border-2 border-dashed w-full rounded-lg h-32 group">
-                    <div className="flex flex-col items-center justify-center pt-7 cursor-pointer">
-                      <svg
-                        className="w-10 h-10 text-purple-400 group-hover:text-purple-600"
-                        fill="none"
-                        stroke="white"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-                        ></path>
-                      </svg>
-                      <p className="lowercase text-sm text-white group-hover:text-purple-600 pt-1 tracking-wider">
-                        Select a photo
-                      </p>
-                    </div>
-                    <input
-                      type="file"
-                      className="hidden"
-                      accept=".jpeg,.jpg,.png,.gif"
-                      {...register('avatar')}
-                      onChange={onFileUpload}
-                    />
-                  </label>
-                </div>
+                {imageURL ? (
+                  <img src={imageURL} width="400px" height="400px" />
+                ) : (
+                  <div className="flex items-center justify-center w-full mt-2">
+                    <label className="flex flex-col border-2 border-dashed w-full rounded-lg h-32 group">
+                      <div className="flex flex-col items-center justify-center pt-7 cursor-pointer">
+                        <svg
+                          className="w-10 h-10 text-purple-400 group-hover:text-purple-600"
+                          fill="none"
+                          stroke="white"
+                          viewBox="0 0 24 24"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                          ></path>
+                        </svg>
+                        <p className="lowercase text-sm text-white group-hover:text-purple-600 pt-1 tracking-wider">
+                          Select a photo
+                        </p>
+                      </div>
+                      <input
+                        type="file"
+                        className="hidden"
+                        accept=".jpeg,.jpg,.png,.gif"
+                        {...register('avatar')}
+                        onChange={onFileUpload}
+                      />
+                    </label>
+                  </div>
+                )}
               </div>
             </div>
             <div className="flex justify-center">
