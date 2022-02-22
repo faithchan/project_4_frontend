1d8de0b894b83b629bee5af93fa59c6b841607cd
faithchan <hannahfaith75@gmail.com>
2022-02-22 10:47:21 +0800

    successfully render isWhitelisted in conetxt

--- components/AccNavigation.tsx
+++ components/AccNavigation.tsx
@@ -13,6 +13,7 @@ interface connectWalletProps {
 
 const AccNavigation = (props: connectWalletProps) => {
   const context = useContext(globalContext)
+  console.log(context)
   const logoutHandler = () => {
     localStorage.removeItem('token')
     context.setLogin(false)
